00405c49fae7063265ebf4c3d54df18ef3544697
Joe Moses <joe@192.168.1.125>
2024-09-07 23:02:12 +0100

    ui

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,6 +1,6 @@
 <div class="container mx-auto pt-10">
     <div x-data="{tab: 'ar'}">
-        <div class="md:flex lg:gap-3 justify-center">
+        <div class="md:flex flex-wrap lg:gap-3 justify-center">
             @foreach($categories as $category)
                 <livewire:image-card
                     :href="route('category', $category)"
--- resources/views/livewire/image-card.blade.php
+++ resources/views/livewire/image-card.blade.php
@@ -1,5 +1,13 @@
-<a wire:navigate.hover href="{{ $href }}" class="bg-slate-950 text-[24px] mb-3 border gun-card w-full bg-cover p-3 h-[300px] md:h-[220px] flex items-end md:w-[420px]"
+<a wire:navigate.hover
+   href="{{ $href }}"
+   class="image-card bg-slate-950 text-[24px] mb-3 border hover:border-brand/50 gun-card w-full p-3 h-[300px] md:h-[220px] flex items-end md:w-[420px]"
    style="background-image: url({{ asset('images/' . $category . '.jpg') }});"
 >
-    {{ $label }}
+
+    <p class="label">
+        {{ $label }}
+    </p>
+    <p class="hover-label">
+        {{ $label }}
+    </p>
 </a>
