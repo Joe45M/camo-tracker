937e27d2226f9b846180d5ce02bf8244026d7b04
Joe Moses <joe@192.168.1.125>
2024-09-08 01:21:46 +0100

    adjustments

--- resources/views/livewire/notice.blade.php
+++ resources/views/livewire/notice.blade.php
@@ -1,4 +1,4 @@
-<div class="container mx-auto mt-10 px-3 lg:px-0">
+<div class="container mx-auto mt-5 px-3 lg:px-0">
     <div class="border border-green-400 bg-green-400/10 p-5">
         <p class="text-[18px]">The website does not have all weapons yet. Once the developers release them, they will be added.</p>
     </div>
