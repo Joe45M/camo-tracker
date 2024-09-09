d08038cdfe999f9390e71e660e3b30c82600c89d
Joe Moses <joe@192.168.1.125>
2024-09-09 01:56:19 +0100

    prettier, readme

--- resources/views/livewire/category.blade.php
+++ resources/views/livewire/category.blade.php
@@ -1,65 +1,69 @@
 <div wire:init="init" class="container mx-auto pt-10">
-    <div class="flex gap-5 justify-between mb-3">
-        <div class="flex gap-3 items-center mb-5">
-{{--            <h1 class="text-2xl font-bold">--}}
-{{--                {{ $name }} <span class="hidden lg:inline">|</span>--}}
-{{--            </h1>--}}
+    <div class="mb-3 flex justify-between gap-5">
+        <div class="mb-5 flex items-center gap-3">
+            {{-- <h1 class="text-2xl font-bold"> --}}
+            {{-- {{ $name }} <span class="hidden lg:inline">|</span> --}}
+            {{-- </h1> --}}
 
-            @foreach($labels as $slug => $label)
-                <a wire:navigate.hover
-                   href="{{ route('category', $slug) }}"
-                   class="{{ $slug === $category ? 'opacity-100 font-bold' : 'opacity-50' }} hidden hover:scale-110 duration-100 lg:block hover:opacity-100
-                       "
-                >{{ $label }}</a>
-                <span class="separator hidden lg:block opacity-50"></span>
+            @foreach ($labels as $slug => $label)
+                <a
+                    wire:navigate.hover
+                    href="{{ route('category', $slug) }}"
+                    class="{{ $slug === $category ? 'font-bold opacity-100' : 'opacity-50' }} hidden duration-100 hover:scale-110 hover:opacity-100 lg:block"
+                >
+                    {{ $label }}
+                </a>
+                <span class="separator hidden opacity-50 lg:block"></span>
             @endforeach
         </div>
 
-        <div class="flex text-xl border self-end border-white/20">
-            <button wire:click="setMode('multiplayer')" class="p-3 {{ $mode === 'multiplayer' ? 'bg-green-500' : 'bg-slate-950' }}">
+        <div class="flex self-end border border-white/20 text-xl">
+            <button
+                wire:click="setMode('multiplayer')"
+                class="{{ $mode === 'multiplayer' ? 'bg-green-500' : 'bg-slate-950' }} p-3"
+            >
                 <i class="fa-solid fa-people-group"></i>
             </button>
-            <button wire:click="setMode('zombies')" class="p-3 {{ $mode === 'zombies' ? 'bg-green-500' : 'bg-slate-950' }}">
+            <button
+                wire:click="setMode('zombies')"
+                class="{{ $mode === 'zombies' ? 'bg-green-500' : 'bg-slate-950' }} p-3"
+            >
                 <i class="fa-solid fa-biohazard"></i>
             </button>
         </div>
     </div>
 
-    <div class="grid lg:grid-cols-5 gap-5">
-        @if($guns)
-            @foreach($guns as $gun)
+    <div class="grid gap-5 lg:grid-cols-5">
+        @if ($guns)
+            @foreach ($guns as $gun)
                 <livewire:gun-card :$gun :key="$gun->id"></livewire:gun-card>
             @endforeach
         @else
-            <div class="h-[150px] bg-[#0f0f0f] border p-3 border-white/20">
-                <div class="w-10 h-5 rounded-md bg-slate-800"></div>
+            <div class="h-[150px] border border-white/20 bg-[#0f0f0f] p-3">
+                <div class="h-5 w-10 rounded-md bg-slate-800"></div>
             </div>
-            <div class="h-[150px] bg-[#0f0f0f] border p-3 border-white/20">
-                <div class="w-10 h-5 rounded-md bg-slate-800"></div>
+            <div class="h-[150px] border border-white/20 bg-[#0f0f0f] p-3">
+                <div class="h-5 w-10 rounded-md bg-slate-800"></div>
             </div>
-            <div class="h-[150px] bg-[#0f0f0f] border p-3 border-white/20">
-                <div class="w-10 h-5 rounded-md bg-slate-800"></div>
+            <div class="h-[150px] border border-white/20 bg-[#0f0f0f] p-3">
+                <div class="h-5 w-10 rounded-md bg-slate-800"></div>
             </div>
-            <div class="h-[150px] bg-[#0f0f0f] border p-3 border-white/20">
-                <div class="w-10 h-5 rounded-md bg-slate-800"></div>
+            <div class="h-[150px] border border-white/20 bg-[#0f0f0f] p-3">
+                <div class="h-5 w-10 rounded-md bg-slate-800"></div>
             </div>
-            <div class="h-[150px] bg-[#0f0f0f] border p-3 border-white/20">
-                <div class="w-10 h-5 rounded-md bg-slate-800"></div>
+            <div class="h-[150px] border border-white/20 bg-[#0f0f0f] p-3">
+                <div class="h-5 w-10 rounded-md bg-slate-800"></div>
             </div>
         @endif
     </div>
 
-    <hr class="mt-10 border-t-white/20">
+    <hr class="mt-10 border-t-white/20" />
 
-
-    <div class="flex flex-wrap  space-between">
-       <div class="h-64 pt-10">
-           <livewire:livewire-pie-chart
-               key="{{ $chart->reactiveKey() }}"
-               :pie-chart-model="$chart"
-           />
-       </div>
-        <div class="text-center h-64 border-l border-l-white/20 pl-5 lg:px-20 items-center flex">
+    <div class="space-between flex flex-wrap">
+        <div class="h-64 pt-10">
+            <livewire:livewire-pie-chart key="{{ $chart->reactiveKey() }}" :pie-chart-model="$chart" />
+        </div>
+        <div class="flex h-64 items-center border-l border-l-white/20 pl-5 text-center lg:px-20">
             <div>
                 <div class="text-center text-[48px]">
                     {{ $stats['completed'] }} /
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,27 +1,18 @@
 <div class="container mx-auto pt-10">
-    <div x-data="{tab: 'ar'}">
-        <div class="md:flex flex-wrap lg:gap-3 justify-center">
+    <div x-data="{ tab: 'ar' }">
+        <div class="flex-wrap justify-center md:flex lg:gap-3">
             <livewire:image-card
                 :href="route('category', 'ar')"
-
                 label="Camo Tracker"
-                :category="'ar'">
-
-            </livewire:image-card>
+                :category="'ar'"
+            ></livewire:image-card>
             <livewire:image-card
                 :href="'https://discord.gg/nWrZTp8N'"
-
                 :shouldNotNavigate="true"
                 label="Join Discord"
-                :category="'discord'">
-
-            </livewire:image-card>
-            <livewire:image-card
-                :href="'/posts/1'"
-                label="Changelog"
-                :category="'change'">
-
-            </livewire:image-card>
+                :category="'discord'"
+            ></livewire:image-card>
+            <livewire:image-card :href="'/posts/1'" label="Changelog" :category="'change'"></livewire:image-card>
         </div>
     </div>
 </div>
