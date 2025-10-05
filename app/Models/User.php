959e0c5d85f786d41969d7319bf30fe291030c89
Joe Moses <joe45moses@gmail.com>
2025-10-05 15:11:04 +0100

    icon tweaks

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@keypress.digital');
+        return str_ends_with($this->email, '@joemoses.co.uk');
     }
 
     public function gunChallenges()
